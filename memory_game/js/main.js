2cb0c510efa46d5c3d1f6d82d7a7204827170d41
Anthony Riojas <riojas19@yahoo.com>
2019-05-20 00:54:35 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,22 @@
 console.log("Up and running!");
-const cards = ['queen', 'queen', 'king', 'king'];
-const cardsInPlay = [];
-const cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-const cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var checkForMatch = function() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}	
+};
+var flipCard = function(cardId) {
+cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId])
 if (cardsInPlay.length === 2) {
+	checkForMatch();
 	if (cardsInPlay[0] === cardsInPlay[1])
 		alert("You found a match!");
 	else alert("Sorry, try again");
 }
+};
+flipCard(0);
+flipCard(2);
